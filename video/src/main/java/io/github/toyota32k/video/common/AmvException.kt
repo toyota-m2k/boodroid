866cc790ef140acdbd56410cc469933f952c31b8
toyota-m2k <toyota.mitsuki@gmail.com>
2025-01-09 17:57:09 +0900

    update libs / delete video lib

--- video/src/main/java/io/github/toyota32k/video/common/AmvException.kt
+++
@@ -1,9 +0,0 @@
-/**
- * 例外クラス
- *
- * @author M.TOYOTA 2018.07.26 Created
- * Copyright © 2018 M.TOYOTA  All Rights Reserved.
- */
-package io.github.toyota32k.video.common
-
-class AmvException @JvmOverloads constructor(msg:String, cause:Throwable?=null) : Exception(msg,cause)
