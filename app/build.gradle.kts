fee52e71c8f902d77f6d0d0a1640e1c62d97e045
toyota-m2k <toyota.mitsuki@gmail.com>
2025-04-24 21:25:22 +0900

    android 16 対応

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -8,12 +8,12 @@ plugins {
 
 android {
     namespace = "io.github.toyota32k.boodroid"
-    compileSdk = 35
+    compileSdk = 36
 
     defaultConfig {
         applicationId = "io.github.toyota32k.boodroid"
         minSdk = 26
-        targetSdk = 35
+        targetSdk = 36
         versionCode = 1
         versionName = "1.12.1"
 
