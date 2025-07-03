a65d9832c14e245e0238989c412bbaa027baeae2
toyota-m2k <toyota.mitsuki@gmail.com>
2025-07-03 14:58:09 +0900

    v1.15.4

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,7 +16,7 @@ android {
         minSdk = 26
         targetSdk = 36
         versionCode = 1
-        versionName = "1.15.3"
+        versionName = "1.15.4"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
