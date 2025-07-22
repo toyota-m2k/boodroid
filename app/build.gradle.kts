d3b64a7241992ad579fdd5424bdc218346d776c0
toyota-m2k <toyota.mitsuki@gmail.com>
2025-07-22 15:10:04 +0900

    utils 4.5.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,7 +16,7 @@ android {
         minSdk = 26
         targetSdk = 36
         versionCode = 1
-        versionName = "2.0.0"
+        versionName = "2.0.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
