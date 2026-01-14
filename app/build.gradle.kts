d4c79ce4d6d23f7ace8da677ce01a5ccbd4d6820
toyota-m2k <toyota.mitsuki@gmail.com>
2026-01-14 14:17:49 +0900

    libs

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,7 +16,7 @@ android {
         minSdk = 26
         targetSdk = 36
         versionCode = 1
-        versionName = "2.1.8"
+        versionName = "2.1.9"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
