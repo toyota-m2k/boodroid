6153a98de53a7e2c7695adae21af8c676f56db84
toyota-m2k <toyota.mitsuki@gmail.com>
2026-06-13 11:07:11 +0900

    v2.5.2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,7 +15,7 @@ configure<ApplicationExtension> {
         minSdk = 26
         targetSdk = 36
         versionCode = 1
-        versionName = "2.5.1"
+        versionName = "2.5.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
