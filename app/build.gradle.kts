b9afeeec337591ba2b1feb89b2a6857b35175eb5
toyota-m2k <toyota.mitsuki@gmail.com>
2026-07-10 10:20:24 +0900

    fix rotation bug (update player lib)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,7 +15,7 @@ configure<ApplicationExtension> {
         minSdk = 26
         targetSdk = 36
         versionCode = 1
-        versionName = "2.6.0"
+        versionName = "2.6.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
